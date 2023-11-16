1c93ed6b495e2b1c1d81104f0de083d1b9d5fb03
Karen Jo <karenfiksdahl@gmail.com>
2023-11-16 15:50:51 +0100

    adds js/index.mjs and register.mjs

---
+++ src/js/handlers/register.mjs
@@ -0,0 +1,12 @@
+const form = document.querySelector("#regForm");
+
+form.addEventListener("submit", (event) => {
+event.preventDefault()
+const form = event.target;
+const formData = new FormData(form);
+const profile = Object.fromEntries(formData.entries())
+
+console.log(profile)
+
+});
+
