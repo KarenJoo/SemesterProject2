5346afe2d5cbd1df9927bac038b1b234001354af
Karen Jo <karenfiksdahl@gmail.com>
2023-11-17 14:22:59 +0100

    adds login.mjs and loginhandler, not done

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,4 +1,6 @@
 // import * as constants from "./api/API.mjs";
 import { setRegisterFormListener } from "./handlers/register.mjs";
+// import { setLoginFormListener } from "./handlers/login.mjs";
 
+// setLoginFormListener();
 setRegisterFormListener();
