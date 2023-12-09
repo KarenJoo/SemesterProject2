d3f4724f1218dafcbda1730fb1b7d6d8a5c3f36f
Karen Jo <karenfiksdahl@gmail.com>
2023-12-09 20:37:34 +0100

    updates all script links to index.mjs, updates router function

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,13 +1,27 @@
 
 //register page
 import { setRegisterFormListener } from "./handlers/register.mjs";
+//login page
 import { setLoginFormListener } from "./handlers/login.mjs";
-import { createListing } from "./listings/index.mjs";
-import { createListingListener } from "./handlers/listings/createListing.mjs"
-import { updateListingListener } from "./handlers/listings/updateListing.mjs";
-import { removeListingListener } from "./handlers/listings/removeListing.mjs";
-import { remove } from "./handlers/storage/index.mjs";
+
+// feed all listings 
+import { allCardsTemplate } from "./pages/allListings.mjs";
+
+// single listing bids
+import { singleCardTemplate } from "./pages/singleListing.mjs";
+import { renderSpecificCard } from "./templates/specificPageTemp.mjs";
+
+// profile page
+import { profileSetUp } from "./api/auth/profile/profileSetup.mjs";
+
+// edit profile
 import { editProfileListener } from "./handlers/profile/editProfileListener.mjs";
+// create listing
+import { createListingListener } from "./handlers/listings/createListing.mjs"
+// update listing
+import { updateListingListener } from "./handlers/listings/updateListing.mjs";
+// remove listing
+import { removeListingListener } from "./handlers/listings/removeListing.mjs";
  
 
 function router() {
@@ -16,6 +30,7 @@ const pathname = location.pathname;
 switch(pathname) {
     case "/":
     console.log("this is the homepage");
+    allCardsTemplate();
     break;
     case "/profile/register/index.html":
     console.log("this is the register page");
@@ -24,6 +39,19 @@ switch(pathname) {
     case "/profile/login/index.html":
     console.log("this is the login page");
     setLoginFormListener();
+    break;
+    case "/listing/specific.html":
+    console.log("this is the specific listing page");
+    singleCardTemplate();
+    renderSpecificCard();
+    break; 
+    case "/profile/index.html":
+    console.log("this is the profile page");
+    profileSetUp();
+    break; 
+    case "/profile/edit/index.html":
+    console.log("this is the edit profile page");
+    editProfileListener();
     break;
     case "/profile/listing/create/index.html":
     console.log("this is the create listing page");
@@ -37,33 +65,9 @@ switch(pathname) {
     console.log("this is the remove listing page");
     removeListingListener();
     break;
-    case "/profile/edit/index.html":
-    console.log("this is the edit profile page");
-    editProfileListener();
-    break;
-    case "/listing/specific.html":
-    console.log("this is the specific listing page");
-    break; 
-    case "/profile/index.html":
-    console.log("this is the profile page");
-    break;
+    
 }
 } 
 
 router(); 
 
-// if (pathname === "/" || pathname === "/index.html") {
-//     console.log("This is the index page")
-// } else if ( pathname === '') {
-//     setRegisterFormListener()
-// } else if (pathname === '/profile/login/index.html') {
-//     setLoginFormListener()
-// } else if (pathname === '/profile/listing/create/index.html') {
-//     createListingListener()
-// } else if (pathname === '/profile/listing/update/index.html') {
-//     updateListingListener()
-// } else if (pathname === '/profile/listing/remove/remove.html') {
-//     removeListingListener()
-// } else if (pathname === '/profile/edit/index.html') {
-//     editProfileListener()
-// }
