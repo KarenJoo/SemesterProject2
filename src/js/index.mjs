28ade06db438c748c2145e78102583d99d6bb48d
Karen Jo <karenfiksdahl@gmail.com>
2023-12-09 19:52:34 +0100

    adds router and restructure file paths

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,5 +1,5 @@
-import * as auth from "./api/auth/index.mjs";
-import * as API from "./api/API.mjs";
+
+//register page
 import { setRegisterFormListener } from "./handlers/register.mjs";
 import { setLoginFormListener } from "./handlers/login.mjs";
 import { createListing } from "./listings/index.mjs";
@@ -7,24 +7,63 @@ import { createListingListener } from "./handlers/listings/createListing.mjs"
 import { updateListingListener } from "./handlers/listings/updateListing.mjs";
 import { removeListingListener } from "./handlers/listings/removeListing.mjs";
 import { remove } from "./handlers/storage/index.mjs";
-
-import { authFetch } from "./listings/authFetch.mjs";
 import { editProfileListener } from "./handlers/profile/editProfileListener.mjs";
  
+
+function router() {
 const pathname = location.pathname;
- 
-if (pathname === "/" || pathname === "/index.html") {
-    console.log("This is the index page")
-} else if ( pathname === '/profile/register/index.html') {
-    setRegisterFormListener()
-} else if (pathname === '/profile/login/index.html') {
-    setLoginFormListener()
-} else if (pathname === '/profile/listing/create/index.html') {
-    createListingListener()
-} else if (pathname === '/profile/listing/update/index.html') {
-    updateListingListener()
-} else if (pathname === '/profile/listing/remove/remove.html') {
-    removeListingListener()
-} else if (pathname === '/profile/edit/index.html') {
-    editProfileListener()
+
+switch(pathname) {
+    case "/":
+    console.log("this is the homepage");
+    break;
+    case "/profile/register/index.html":
+    console.log("this is the register page");
+    setRegisterFormListener();
+    break;
+    case "/profile/login/index.html":
+    console.log("this is the login page");
+    setLoginFormListener();
+    break;
+    case "/profile/listing/create/index.html":
+    console.log("this is the create listing page");
+    createListingListener();
+    break;
+    case "/profile/listing/update/index.html":
+    console.log("this is the update listing page");
+    updateListingListener();
+    break;
+    case "/profile/listing/remove/remove.html":
+    console.log("this is the remove listing page");
+    removeListingListener();
+    break;
+    case "/profile/edit/index.html":
+    console.log("this is the edit profile page");
+    editProfileListener();
+    break;
+    case "/listing/specific.html":
+    console.log("this is the specific listing page");
+    break; 
+    case "/profile/index.html":
+    console.log("this is the profile page");
+    break;
 }
+} 
+
+router(); 
+
+// if (pathname === "/" || pathname === "/index.html") {
+//     console.log("This is the index page")
+// } else if ( pathname === '') {
+//     setRegisterFormListener()
+// } else if (pathname === '/profile/login/index.html') {
+//     setLoginFormListener()
+// } else if (pathname === '/profile/listing/create/index.html') {
+//     createListingListener()
+// } else if (pathname === '/profile/listing/update/index.html') {
+//     updateListingListener()
+// } else if (pathname === '/profile/listing/remove/remove.html') {
+//     removeListingListener()
+// } else if (pathname === '/profile/edit/index.html') {
+//     editProfileListener()
+// }
